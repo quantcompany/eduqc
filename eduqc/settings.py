40eec5c3c2a37a83b6bbd9cd5911e04a159979df
aaj <mando.alvarado.jose@gmail.com>
2016-11-15 08:31:09 -0600

    Set allowed_hosts setting for production

--- eduqc/settings.py
+++ eduqc/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['edu.quantcompany.com', '198.199.111.154']
 
 
 # Application definition
